7c16e9b66825b1d0bd4acb3404282cae199ab11a
betty2310 <huynh.dh2310@gmail.com>
2021-10-16 18:05:22 +0700

    feat(student-manager): add funtion add student sort by grade

--- w3/studentManager.c
+++ w3/studentManager.c
@@ -5,19 +5,80 @@
 #define ID_LENGTH 10
 #define NAME_LENGTH 255
 
-struct data {
+struct student
+{
     char id[ID_LENGTH];
     char name[NAME_LENGTH];
     int grade;
 };
 
-typedef struct Student_t {
-    struct data Data;
-    struct Student_t *next;
-} Student;
+typedef struct Node
+{
+    struct student Data;
+    struct Node *next;
+} Node;
 
+void addStudent(Node **head, struct student sv) //student add default by grade
+{
+    Node *cur = (Node *)malloc(sizeof(Node));
+    if (*head == NULL)
+    {
+        cur->Data = sv;
+        cur->next = NULL;
+        *head = cur;
+        return;
+    }
+    if (sv.grade >= (*head)->Data.grade)
+    {
+        cur->Data = sv;
+        cur->next = (*head);
+        *head = cur;
+        return;
+    }
+    cur = *head;
+    Node *new_sv = (Node *)malloc(sizeof(Node));
+    // find prev index > grade
+    while (cur->next != NULL && sv.grade <= cur->next->Data.grade)
+    {
+        cur = cur->next;
+    }
+    new_sv->Data = sv;
+    new_sv->next = cur->next;
+    cur->next = new_sv;
+    return;
+
+    return;
+}
+
+void printList(Node *head)
+{
+    for (Node *cur = head; cur != NULL; cur = cur->next)
+    {
+        printf("%s %s %d\n", cur->Data.id, cur->Data.name, cur->Data.grade);
+    }
+}
 int main(int argc, char *argv[])
 {
-    
+    Node *head = NULL;
+    struct student sv1 = {"20201234", "Pham Hong An", 12};
+    struct student sv2 = {"20205091", "Le Phuong Anh", 13};
+    struct student sv3 = {"20201434", "Duong Huu Huynh", 9};
+    struct student sv4 = {"20204354", "Vu Hoang ANh", 11};
+    struct student sv5 = {"20206873", "Hoang Khai Nga", 10};
+    struct student sv6 = {"20205348", "Le Hong Van", 12};
+    struct student sv7 = {"20201234", "Bac Kieu Phong", 8};
+    struct student sv8 = {"20209876", "Au Duong Co", 14};
+
+    addStudent(&head, sv1);
+    addStudent(&head, sv2);
+    addStudent(&head, sv3);
+    addStudent(&head, sv4);
+    addStudent(&head, sv5);
+    addStudent(&head, sv6);
+    addStudent(&head, sv7);
+    addStudent(&head, sv8);
+
+    printList(head);
+
     return 0;
 }
