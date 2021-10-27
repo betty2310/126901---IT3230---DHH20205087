d02cfc70e994eb9c335d3bf8b5c6a18b972c20ef
betty2310 <huynh.dh2310@gmail.com>
2021-10-27 13:35:43 +0700

    feat(format): use extern queue to all file .c

--- w5/toDoList/src/main.c
+++ w5/toDoList/src/main.c
@@ -5,9 +5,11 @@
 #include "toDo.h"
 #include "queue.h"
 
+struct Queue *q;
+
 int main()
 {
-    struct Queue *q = createQueue();
+    q = createQueue();
     int ch = 1;
     do
     {
@@ -16,41 +18,13 @@ int main()
         scanf("%d", &ch);
         getchar();
         if (ch == 1)
-        {
-            printf("\tAdd todo:\n");
-            time_t t = time(NULL);
-            struct tm tm = *localtime(&t);
-            char s[255];
-            printf("\tEnter script: ");
-            fgets(s, 255, stdin);
-            int n = strlen(s);
-            s[n - 1] = '\0';
-            int time;
-            printf("\tEnter time: ");
-            scanf("%d", &time);
-            ToDo io;
-            strcpy(io.script, s);
-            io.time = time;
-            io.tm = tm;
-            enQueue(q, io);
-        }
+            addToDoList();
         if (ch == 2)
-        {
-            printf("\tLam xong viec dau tien trong danh sach!\n");
-            deQueue(q);
-        }
+            removeFirstToDo();
         if (ch == 3)
-        {
-            printf("Xem viec can lam bay gio: \n");
-            printf("Thoi gian tao\t\tNoi dung\tThoi gian lam\n");
-            printf("%d-%02d-%02d %02d:%02d:%02d\t", q->front->key.tm.tm_year + 1900, q->front->key.tm.tm_mon + 1, q->front->key.tm.tm_mday, q->front->key.tm.tm_hour, q->front->key.tm.tm_min, q->front->key.tm.tm_sec);
-            printf("%s\t\t%d\n\n", q->front->key.script, q->front->key.time);
-        }
+            seeFirstToDo();
         if (ch == 0)
-        {
             printf("<----EXIT----->!\n");
-        }
-
     } while (ch);
     return 0;
 }
