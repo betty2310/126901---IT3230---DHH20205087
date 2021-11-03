1e1c2bdd25e9e8756b1b6a0550ecfe04752f3fb9
betty2310 <huynh.dh2310@gmail.com>
2021-11-03 12:51:59 +0700

    feat(studentManager): initial commit

---
+++ w6/studentmanager/build/CMakeFiles/studentmanager.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/studentmanager.dir/main.c.o"
+  "CMakeFiles/studentmanager.dir/student.c.o"
+  "studentmanager"
+  "studentmanager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/studentmanager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ w6/studentmanager/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/betty/Dev/CProgramming-IT3230-126901/w6/studentmanager
+# Build directory: /home/betty/Dev/CProgramming-IT3230-126901/w6/studentmanager/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
