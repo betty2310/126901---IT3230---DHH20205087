bad0cd2a93dade9c6b6088743f145dd935c06bbf
betty2310 <huynh.dh2310@gmail.com>
2021-11-08 20:06:10 +0700

    feat(*): update

---
+++ w7/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/betty/Dev/IT3230/w7
+# Build directory: /home/betty/Dev/IT3230/w7/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
