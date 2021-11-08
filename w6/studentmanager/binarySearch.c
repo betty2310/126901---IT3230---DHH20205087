8c19817af2795cf1ed31f8366587a5e40bb92459
betty2310 <huynh.dh2310@gmail.com>
2021-11-09 00:11:57 +0700

    feat(*): first update (findStudentByGrade)

--- w6/studentmanager/binarySearch.c
+++ w6/studentmanager/binarySearch.c
@@ -4,10 +4,44 @@
 
 #include "student.h"
 
+Node *nodeMiddle(Node *head, Node *tail)
+{
+    Node *middle = head;
+    Node *current = head;
+
+    // current chay nhanh hon middle 2 lan
+    while (current != tail)
+    {
+        current = current->next;
+        if (current != tail)
+        {
+            current = current->next;
+        }
+        middle = middle->next;
+    }
+    return middle;
+}
+
 Node *findStudentByGradeUseBinarySearch(Node *head, int grade, int n)
 {
-    //Tai sao lai phai dung tim kiem nhi phan khi ma no khong tac dung gi?
+    Node *start = head;
+    Node *last = NULL;
+    while (last != start)
+    {
+        Node *mid = nodeMiddle(start, last);
+        if (mid == NULL)
+            return NULL;
+        if (mid->Data.grade == grade)
+            return mid;
+        if (mid->Data.grade > grade)
+        {
+            start = mid->next;
+        }
+        if (mid->Data.grade < grade)
+        {
+            last = mid;
+        }
+    }
+    return NULL;
 }
-Node *findStudentByNameUseBinarySearch(Node *head, char *name, int n)
-{
-}
+
