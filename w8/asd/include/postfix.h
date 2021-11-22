6bb9b054e474e9d0213585e57dc3cdcf235139bb
betty2310 <huynh.dh2310@gmail.com>
2021-11-22 11:42:36 +0700

    feat(*): change folder name

--- w8/asd/include/postfix.h
+++
@@ -1,3 +0,0 @@
-int isOperator(char c);
-int order(char ch);
-void infixToPostfix(char *infix, char *postfix);
--- w8/asd/include/tree.h
+++
@@ -1,8 +0,0 @@
-typedef struct NodeTree
-{
-    char data;
-    struct NodeTree *left, *right;
-} NodeT;
-extern NodeT *root;
-NodeT *makeNode(char n);
-void showTree(NodeT *root);
