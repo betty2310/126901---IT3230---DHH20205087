6e86641591f6a356dcd442b82f81f7980f667501
betty2310 <huynh.dh2310@gmail.com>
2021-12-27 22:58:59 +0700

    feat(*): test commitizen

--- w10/main.c
+++ w10/main.c
@@ -8,13 +8,11 @@
 #define N 500
 #define MAX_RANGE 10000
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     FILE *fn;
     int a[N + 1];
     fn = fopen("number.txt", "w");
-    for (int i = 0; i < N; ++i)
-    {
+    for (int i = 0; i < N; ++i) {
         int num = (rand() % MAX_RANGE) + 1;
         fprintf(fn, "%d ", num);
         a[i] = num;
@@ -28,70 +26,63 @@ int main(int argc, char *argv[])
     printf("|\t5. Heap Sort        |\n");
     printf("\nEnter choice: ");
     scanf("%d", &c);
-    switch (c)
-    {
-    case 1:
-    {
-        printf("You select insertionSort, ");
-        clock_t t;
-        t = clock();
-        insertionSort(a, N);
-        t = clock() - t;
-        double time_taken = ((double)t) / 1000000; // in seconds
-        printf("took %lf s", time_taken);
-        break;
-    }
-    case 2:
-    {
-        printf("\nYou select selectionSort, ");
-        clock_t t;
-        t = clock();
-        selectionSort(a, N);
-        t = clock() - t;
-        double time_taken = ((double)t) / 1000000; // in seconds
-        printf("took %lf s", time_taken);
-        break;
-    }
-    case 3:
-    {
-        printf("\nYou select mergeSort, ");
-        clock_t t;
-        t = clock();
-        mergeSort(a, 0, N);
-        t = clock() - t;
-        double time_taken = ((double)t) / 1000000; // in seconds
-        printf("took %lf s", time_taken);
-        break;
-    }
-    case 4:
-    {
-        printf("\nYou select quickSort, ");
-        clock_t t;
-        t = clock();
-        quickSort(a, 0, N - 1);
-        t = clock() - t;
-        double time_taken = ((double)t) / 1000000; // in seconds
-        printf("took %lf s", time_taken);
-        break;
-    }
-    case 5:
-    {
-        printf("You select heapSort, ");
-        clock_t t;
-        t = clock();
-        heapSort(a, N);
-        t = clock() - t;
-        double time_taken = ((double)t) / 1000000; // in seconds
-        printf("took %lf s", time_taken);
-        break;
-    }
-    default:
-        printf("Error!\n");
-        break;
+    switch (c) {
+        case 1: {
+            printf("You select insertionSort, ");
+            clock_t t;
+            t = clock();
+            insertionSort(a, N);
+            t = clock() - t;
+            double time_taken = ((double)t) / 1000000;  // in seconds
+            printf("took %lf s", time_taken);
+            break;
+        }
+        case 2: {
+            printf("\nYou select selectionSort, ");
+            clock_t t;
+            t = clock();
+            selectionSort(a, N);
+            t = clock() - t;
+            double time_taken = ((double)t) / 1000000;  // in seconds
+            printf("took %lf s", time_taken);
+            break;
+        }
+        case 3: {
+            printf("\nYou select mergeSort, ");
+            clock_t t;
+            t = clock();
+            mergeSort(a, 0, N);
+            t = clock() - t;
+            double time_taken = ((double)t) / 1000000;  // in seconds
+            printf("took %lf s", time_taken);
+            break;
+        }
+        case 4: {
+            printf("\nYou select quickSort, ");
+            clock_t t;
+            t = clock();
+            quickSort(a, 0, N - 1);
+            t = clock() - t;
+            double time_taken = ((double)t) / 1000000;  // in seconds
+            printf("took %lf s", time_taken);
+            break;
+        }
+        case 5: {
+            printf("You select heapSort, ");
+            clock_t t;
+            t = clock();
+            heapSort(a, N);
+            t = clock() - t;
+            double time_taken = ((double)t) / 1000000;
+            printf("took %lf s", time_taken);
+            break;
+        }
+        default:
+            printf("Error!\n");
+            break;
     }
     fprintf(fn, "\n\n---------\n\n");
-    for (int i = 0; i < N; ++i)
-    {
+    for (int i = 0; i < N; ++i) {
         fprintf(fn, "%d ", a[i]);
     }
     fclose(fn);
