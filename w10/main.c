951182474f558eac693e8b7df9280251b05492ba
betty2310 <huynh.dh2310@gmail.com>
2021-12-27 11:47:54 +0700

    style(formatting): ----

--- w10/main.c
+++ w10/main.c
@@ -20,13 +20,19 @@ int main(int argc, char *argv[])
         a[i] = num;
     }
     int c;
-    printf("Enter choice: ");
+    printf("|\t  ----MENU----      |\n");
+    printf("|\t1. Insertion Sort   |\n");
+    printf("|\t2. Selection Sort   |\n");
+    printf("|\t3. Merge Sort       |\n");
+    printf("|\t4. Quick Sort       |\n");
+    printf("|\t5. Heap Sort        |\n");
+    printf("\nEnter choice: ");
     scanf("%d", &c);
     switch (c)
     {
     case 1:
     {
-        printf("You select insertion Sort\n");
+        printf("You select insertionSort, ");
         clock_t t;
         t = clock();
         insertionSort(a, N);
@@ -37,7 +43,7 @@ int main(int argc, char *argv[])
     }
     case 2:
     {
-        printf("\nYou select selection Sort\n");
+        printf("\nYou select selectionSort, ");
         clock_t t;
         t = clock();
         selectionSort(a, N);
@@ -48,7 +54,7 @@ int main(int argc, char *argv[])
     }
     case 3:
     {
-        printf("\nYou select merge Sort\n");
+        printf("\nYou select mergeSort, ");
         clock_t t;
         t = clock();
         mergeSort(a, 0, N);
@@ -59,10 +65,21 @@ int main(int argc, char *argv[])
     }
     case 4:
     {
-        printf("\nYou select quick Sort\n");
+        printf("\nYou select quickSort, ");
         clock_t t;
         t = clock();
         quickSort(a, 0, N - 1);
+        t = clock() - t;
+        double time_taken = ((double)t) / 1000000; // in seconds
+        printf("took %lf s", time_taken);
+        break;
+    }
+    case 5:
+    {
+        printf("You select heapSort, ");
+        clock_t t;
+        t = clock();
+        heapSort(a, N);
         t = clock() - t;
         double time_taken = ((double)t) / 1000000; // in seconds
         printf("took %lf s", time_taken);
