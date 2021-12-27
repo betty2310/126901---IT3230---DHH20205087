4bc6fc7ccc76af9bc1b064d02738180e9bddaada
betty2310 <huynh.dh2310@gmail.com>
2021-12-27 11:29:45 +0700

    refactor(*): h and .c, right?

---
+++ w10/include/sort.h
@@ -0,0 +1,10 @@
+void insertionSort(int a[], int size);
+void selectionSort(int a[], int size);
+
+// merge sort
+void merge(int arr[], int l, int m, int r);
+void mergeSort(int a[], int left, int right);
+
+//quick sort
+int partition(int a[], int left, int right);
+void quickSort(int a[], int left, int right);
